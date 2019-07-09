2adc00a122c7a3b4c26455aacaf0277ecfe93b51
mocmeo <vdev.lamnn@gmail.com>
2019-07-09 21:45:49 +0700

    Refactor for cleaner code

--- content.js
+++ content.js
@@ -1,18 +1,15 @@
 console.log("Chrome extension go?");
 
-window.addEventListener("load", function() {
+window.addEventListener("load", () => {
 	let panel = document.querySelector("ul.action-list");
-	let container = document.createElement("li");
-
-	let downloadBtn = document.querySelector("li a[title='Tải xuống']");
-	let vipBtn = downloadBtn.cloneNode(true);
-	vipBtn.setAttribute("title", "Tải");
-	vipBtn.querySelector("span").innerHTML = "VIP (320kbps)";
-	container.appendChild(vipBtn);
 
 	if (panel) {
-		// panel.appendChild(container);
-		panel.insertBefore(container, panel.lastChild);
-		console.log(panel);
+		// button Tải xuống
+		let downloadBtn = document.querySelector("ul.action-list").children[4];
+		let vipBtn = downloadBtn.cloneNode(true);
+		vipBtn.setAttribute("title", "Tải");
+		vipBtn.querySelector("span").innerHTML = "VIP (320kbps)";
+
+		panel.insertBefore(vipBtn, panel.lastChild);
 	}
 });
