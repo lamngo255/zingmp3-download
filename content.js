8b6fce8480abc3c108c36956679c38ba9d4db7b8
mocmeo <vdev.lamnn@gmail.com>
2019-07-10 11:23:13 +0700

    Send data back from background script, get link mp3

--- content.js
+++ content.js
@@ -1,30 +1,20 @@
-const getDownloadLink = async songUrl => {
-	// https://zing-mp3.glitch.me/?url=https://mp3.zing.vn/bai-hat/Hoc-Tieng-Meo-Keu-Xuan-Tai-Ly-Phuong-Thanh/ZW9CZOFF.html
-	// console.log(url);
-	// let response = await fetch(`${baseUrl}${url}`);
-	// return response;
+const songUrl = window.location.href;
 
-	chrome.runtime.sendMessage(
-		{ contentScriptQuery: "queryZingMp3", url: songUrl },
-		data => {
-			return data;
+chrome.runtime.sendMessage(
+	{ contentScriptQuery: "queryZingMp3", url: songUrl },
+	songData => {
+		let panel = document.querySelector("ul.action-list");
+		let linkVip = songData.source.audio[320].download;
+		let songTitle = songData.title;
+
+		if (panel) {
+			let downloadBtn = document.querySelector("ul.action-list").children[4];
+			let vipBtn = downloadBtn.cloneNode(true);
+			vipBtn.setAttribute("title", "Tải");
+			vipBtn.querySelector("span").innerHTML = "VIP (320kbps)";
+			vipBtn.querySelector("a").setAttribute("href", linkVip);
+
+			panel.insertBefore(vipBtn, panel.lastChild);
 		}
-	);
-};
-
-window.addEventListener("load", async () => {
-	let panel = document.querySelector("ul.action-list");
-	let link = window.location.href;
-	let response = await getDownloadLink(link);
-	console.log(response);
-
-	if (panel) {
-		// button Tải xuống
-		let downloadBtn = document.querySelector("ul.action-list").children[4];
-		let vipBtn = downloadBtn.cloneNode(true);
-		vipBtn.setAttribute("title", "Tải");
-		vipBtn.querySelector("span").innerHTML = "VIP (320kbps)";
-
-		panel.insertBefore(vipBtn, panel.lastChild);
 	}
-});
+);
